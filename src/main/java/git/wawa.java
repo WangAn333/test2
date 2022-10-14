30563ba8bc9016f021d90813b5ae378c7af54bef
tom_glb <goodMorning_pro@atguigu.com>
2022-10-14 10:13:13 +0800

    hotfix commit2

--- src/main/java/git/wawa.java
+++ src/main/java/git/wawa.java
@@ -5,5 +5,6 @@ public class wawa {
         System.out.println("hello wa!");
         System.out.println("hello wa!2");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
     }
 }
